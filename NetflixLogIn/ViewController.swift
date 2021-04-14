3433fa6a652b68d051961cb2e76161c569b25931
Billy Huang <j28898601@gmail.com>
2021-04-14 17:27:44 +0800

    Netflix log in

--- NetflixLogIn/ViewController.swift
+++ NetflixLogIn/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changePage(_ sender: UIPageControl) {
+        let point = CGPoint(x: scrollView.bounds.width * CGFloat(sender.currentPage), y: 0)
+        scrollView.setContentOffset(point, animated: true)
+    }
+    
 }
 
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        pageControl.currentPage = Int(page)
+    }
+}
